b30b4637e20605edef18d91abd8f2f78e00d3c64
Hajime-Suzuki <hajime.doublebass@gmail.com>
2019-11-23 14:52:19 +0100

    jest config

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  resetMocks: true,
-  testMatch: [ '**/__tests__/**/*.test.ts' ],
-  globals: { diagnostics: { warnOnly: true } },
-  moduleNameMapper: { '^@modules/(.+)': '<rootDir>/src/modules/$1' },
-}
